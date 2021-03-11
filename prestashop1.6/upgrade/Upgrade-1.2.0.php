e535e29cd2b3dc47109bf944d69726e68165cb80
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-03-11 23:01:37 +0800

    updated the upgrade script to insert records for the new widget config entry(TAWKTO_WIDGET_CURRENT) and remove the old config forpage id and widget id

--- prestashop1.6/upgrade/Upgrade-1.2.0.php
+++ prestashop1.6/upgrade/Upgrade-1.2.0.php
@@ -24,6 +24,33 @@ if (!defined('_PS_VERSION_')) {
 function upgrade_module_1_2_0()
 {
     $db = Db::getInstance();
+
+    // remove the suffix first on the config key names.
+    $remove_suffix_result = remove_suffix($db);
+    if (!$remove_suffix_result) {
+        return false;
+    }
+
+    // force reload cache
+    Configuration::loadConfiguration();
+
+    // insert the new records for TAWKTO_WIDGET_CURRENT.
+    $insert_records_result = insert_records();
+    if (!$insert_records_result) {
+        return false;
+    }
+
+    // delete records for TAWKTO_WIDGET_PAGE_ID & TAWKTO_WIDGET_WIDGET_ID
+    $remove_extras_result = remove_extras();
+    if (!$remove_extras_result) {
+        return false;
+    }
+
+    return true;
+}
+
+function remove_suffix($db)
+{
     $keys = array(
         TawkTo::TAWKTO_WIDGET_PAGE_ID,
         TawkTo::TAWKTO_WIDGET_WIDGET_ID,
@@ -54,4 +81,59 @@ function upgrade_module_1_2_0()
     $result = $db->execute(join(' ', $sql)); //returns boolean value
 
     return $result;
+}
+
+function insert_records()
+{
+    $res = true;
+
+    // modify global first
+    $res &= modify_widget();
+
+    $shop_ids = Shop::getCompleteListOfShopsID();
+
+    $updated_groups = array();
+    foreach ($shop_ids as $shop_id) {
+        $shop_group_id = (int)Shop::getGroupFromShop($shop_id);
+
+        if (!in_array($shop_group_id, $updated_groups)) {
+            // update the group config
+            $res &= modify_widget($shop_group_id);
+            $updated_groups[] = $shop_group_id;
+        }
+
+        // update the shop config
+        $res &= modify_widget($shop_group_id, $shop_id);
+    }
+
+    return $res;
+}
+
+function modify_widget($shop_group_id = null, $shop_id = null)
+{
+    if (isset($shop_id)) {
+        Shop::setContext(Shop::CONTEXT_SHOP, $shop_id);
+    } elseif (isset($shop_group_id)) {
+        Shop::setContext(Shop::CONTEXT_GROUP, $shop_group_id);
+    } else {
+        Shop::setContext(Shop::CONTEXT_ALL);
+    }
+
+    $page_id = Configuration::get(TawkTo::TAWKTO_WIDGET_PAGE_ID, null, $shop_group_id, $shop_id);
+    $widget_id = Configuration::get(TawkTo::TAWKTO_WIDGET_WIDGET_ID, null, $shop_group_id, $shop_id);
+    return Configuration::updateValue(
+        TawkTo::TAWKTO_WIDGET_CURRENT,
+        $page_id.':'.$widget_id,
+        false,
+        $shop_group_id,
+        $shop_id
+    );
+}
+
+function remove_extras()
+{
+    // remove TAWKTO_WIDGET_PAGE_ID and TAWKTO_WIDGET_WIDGET_ID records
+    $res = Configuration::deleteByName(TawkTo::TAWKTO_WIDGET_PAGE_ID);
+    $res &= Configuration::deleteByName(TawkTo::TAWKTO_WIDGET_WIDGET_ID);
+    return $res;
 }
