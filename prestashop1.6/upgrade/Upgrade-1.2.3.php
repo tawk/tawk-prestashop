1cfb5884bf3469c63b33242e56af3210da73da3a
eug-L <eugene4198@gmail.com>
2024-08-26 09:29:35 +0800

    add php-cs-fixer and prestashop standard

--- prestashop1.6/upgrade/Upgrade-1.2.3.php
+++ prestashop1.6/upgrade/Upgrade-1.2.3.php
@@ -17,94 +17,95 @@
  * @copyright Copyright (c) 2014-2022 tawk.to
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 if (!defined('_PS_VERSION_')) {
-  exit;
+    exit;
 }
 
 /**
  * Upgrade entry point
  *
- * @return boolean
+ * @return bool
  */
-function upgrade_module_1_2_3() {
-  // update the records for TAWKTO_WIDGET_OPTS.
-  $update_records_result = update_records();
-  if (!$update_records_result) {
-    return FALSE;
-  }
+function upgrade_module_1_2_3()
+{
+    // update the records for TAWKTO_WIDGET_OPTS.
+    $update_records_result = update_records();
+    if (!$update_records_result) {
+        return false;
+    }
 
-  return TRUE;
+    return true;
 }
 
 /**
  * Update records
  *
- * @return boolean
+ * @return bool
  */
-function update_records() {
-  $res = TRUE;
+function update_records()
+{
+    $res = true;
 
-  // modify global first
-  $res &= update_visibility_opts();
+    // modify global first
+    $res &= update_visibility_opts();
 
-  $shop_ids = Shop::getCompleteListOfShopsID();
+    $shop_ids = Shop::getCompleteListOfShopsID();
 
-  $updated_groups = [];
-  foreach ($shop_ids as $shop_id) {
-    $shop_group_id = (int) Shop::getGroupFromShop($shop_id);
+    $updated_groups = [];
+    foreach ($shop_ids as $shop_id) {
+        $shop_group_id = (int) Shop::getGroupFromShop($shop_id);
 
-    if (!in_array($shop_group_id, $updated_groups)) {
-      // update the group config
-      $res &= update_visibility_opts($shop_group_id);
-      $updated_groups[] = $shop_group_id;
+        if (!in_array($shop_group_id, $updated_groups)) {
+            // update the group config
+            $res &= update_visibility_opts($shop_group_id);
+            $updated_groups[] = $shop_group_id;
+        }
+
+        // update the shop config
+        $res &= update_visibility_opts($shop_group_id, $shop_id);
     }
 
-    // update the shop config
-    $res &= update_visibility_opts($shop_group_id, $shop_id);
-  }
-
-  return $res;
+    return $res;
 }
 
 /**
  * Update visibility options
  *
- * @param null|integer $shop_group_id Shop group ID.
- * @param null|integer $shop_id       Shop ID.
- * @return boolean
+ * @param int|null $shop_group_id shop group ID
+ * @param int|null $shop_id shop ID
+ *
+ * @return bool
  */
-function update_visibility_opts($shop_group_id = NULL, $shop_id = NULL) {
-  if (isset($shop_id)) {
-    Shop::setContext(Shop::CONTEXT_SHOP, $shop_id);
-  }
-  elseif (isset($shop_group_id)) {
-    Shop::setContext(Shop::CONTEXT_GROUP, $shop_group_id);
-  }
-  else {
-    Shop::setContext(Shop::CONTEXT_ALL);
-  }
+function update_visibility_opts($shop_group_id = null, $shop_id = null)
+{
+    if (isset($shop_id)) {
+        Shop::setContext(Shop::CONTEXT_SHOP, $shop_id);
+    } elseif (isset($shop_group_id)) {
+        Shop::setContext(Shop::CONTEXT_GROUP, $shop_group_id);
+    } else {
+        Shop::setContext(Shop::CONTEXT_ALL);
+    }
 
-  $opts = Configuration::get(TawkTo::TAWKTO_WIDGET_OPTS, NULL, $shop_group_id, $shop_id);
+    $opts = Configuration::get(TawkTo::TAWKTO_WIDGET_OPTS, null, $shop_group_id, $shop_id);
 
-  if (!$opts) {
-    return FALSE;
-  }
+    if (!$opts) {
+        return false;
+    }
 
-  $opts = json_decode($opts);
+    $opts = json_decode($opts);
 
-  if (isset($opts->show_oncustom) && is_array($opts->show_oncustom) && $opts->show_oncustom === []) {
-    $opts->show_oncustom = json_encode([]);
-  }
+    if (isset($opts->show_oncustom) && is_array($opts->show_oncustom) && $opts->show_oncustom === []) {
+        $opts->show_oncustom = json_encode([]);
+    }
 
-  if (isset($opts->hide_oncustom) && is_array($opts->hide_oncustom) && $opts->hide_oncustom === []) {
-    $opts->hide_oncustom = json_encode([]);
-  }
+    if (isset($opts->hide_oncustom) && is_array($opts->hide_oncustom) && $opts->hide_oncustom === []) {
+        $opts->hide_oncustom = json_encode([]);
+    }
 
-  return Configuration::updateValue(
+    return Configuration::updateValue(
         TawkTo::TAWKTO_WIDGET_OPTS,
         json_encode($opts),
-        FALSE,
+        false,
         $shop_group_id,
         $shop_id
     );
